1c6845c1263eb2847b0ca7a59147759bc35dc5bb
yuechengu <gyc822@qq.com>
2022-03-14 17:06:31 +0800

    3/14

---
+++ src/main/java/com/learning/UtxoModelApplication.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtxoModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtxoModelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/UtxoModelApplicationTests.java
@@ -0,0 +1,43 @@
+package com.learning;
+
+import com.google.common.hash.Hashing;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+
+
+@SpringBootTest
+class UtxoModelApplicationTests {
+
+    @Autowired
+    DataSource dataSource;
+    @Test
+    void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
+        System.out.println(dataSource.getConnection());
+    }
+
+    @Test
+    void sha256Test() {
+        String hash = Hashing.sha256().hashString("testing", StandardCharsets.UTF_8).toString();
+        System.out.println(hash);
+    }
+
+    @Test
+    void sha1Test() {
+        String hash = Hashing.sha1().hashString("testing", StandardCharsets.UTF_8).toString();
+        System.out.println(hash);
+    }
+
+    @Test
+    void sipHash24Test() {
+        String hash = Hashing.sipHash24().hashString("testing", StandardCharsets.UTF_8).toString();
+        System.out.println(hash);
+    }
+
+
+}
